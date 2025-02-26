c0ea427ae2a996448a281959e7cf5ffa893faffd
anantin235768 <anantin23-4005@cca.edu.ph>
2025-02-26 18:45:54 +0800

    html form

---
+++ register.php
@@ -0,0 +1,39 @@
+<html>
+    <head>
+        <title> From submission</title>
+</head>
+<body>
+    <form action="action.php" method="post">
+        <table border="1">
+            <tr>
+                <th>Quetion</th>
+                <th>Answer</th>
+</tr>
+<tr>
+    <td>Name</td>
+    <td><Input type="text"> name="name"</td>
+</tr>
+<tr>
+    <td>Gender</td>
+    <td><input type="radio" name ="gender"value="male"> Male </td>
+    <td><input type="radio" name ="gender"value="female">female</td>
+</td>
+</tr>
+<tr>
+    <td> Country</td>
+    <td>
+        <select name="country">
+            <option value="PH">Philippines </option>
+            <option value="Aus"Austrial> </option>
+            <option value="Us"United state> </option>
+</select>
+</td>
+</tr>
+<td> conspan="2">
+    <Input type = "submit"value="save">
+</td>
+</tr>
+        </table>
+    </form>
+</body>
+</html>
